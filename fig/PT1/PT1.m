fd90a8c72277c4af14c0231addee6c7c58da1a15
Alexander Wilms <f.alexander.wilms@gmail.com>
2015-12-09 20:55:25 +0100

    Only ignore thesis.pdf

--- fig/PT1/PT1.m
+++ fig/PT1/PT1.m
@@ -1,11 +1,17 @@
 clc; close all;
 pkg load control
+graphics_toolkit('qt')
 s=tf('s')
 g=1/(s+1)
 bode(g)
-a=subplot(2,1,1)
-grid off
-a=subplot(2,1,2)
-grid off
+%a=subplot(2,1,1)
+%grid off
+%title('\alpha Bode-Diagramm eines PT_1');
+%a=subplot(2,1,2)
+%grid off
 legend off
+set(findall(gcf,'-property','linewidth'), 'linewidth', 1.5) 
+set(findall(gcf,'-property','FontName'),'FontName','arial');
+set(findall(gcf,'-property','FontSize'),'FontSize',12);
 print('PT1-octave','-dpdf')
+close
